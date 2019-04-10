f774b766c7d4128c4f554a7e223ddacb128c5192
labuser <labuser@lab>
2019-04-10 13:11:57 +0100

    things are still fucked but at least the charts are sort of displaying (though without loading spinners)

---
+++ bazel_setup.sh
@@ -0,0 +1,2 @@
+chmod +x bazel-0.19.1-installer-linux-x86_64.sh &&
+./bazel-0.19.1-installer-linux-x86_64.sh --user
--- run_demo.sh
+++ run_demo.sh
@@ -1,3 +1,2 @@
-bazel run //paramplot:paramplot_demo
-
+bazel run //paramplot:paramplot_demo &&
 bazel run //gr_tensorboard -- --logdir=/tmp/paramplotdemo --runParamsPath=/home/labuser/ranjeevm/tensorboard-extensions/runparams.json

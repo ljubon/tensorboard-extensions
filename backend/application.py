8dd517ae0937422605276eb85ea5360153c45fd4
labuser <labuser@lab>
2019-05-14 09:47:50 +0100

    upgraded to version 18

--- backend/application.py
+++ backend/application.py
@@ -8,7 +8,7 @@ from tensorboard.backend.event_processing import plugin_event_multiplexer
 
 def gr_tensorboard_wsgi(flags, plugin_loaders, assets_zip_provider):
     size_guidance = {plugin_event_accumulator.TENSORS: 100}
-    run_path_map = _getRunPathMapFromLogdir(flags.logdir, 50)
+    run_path_map = _getRunPathMapFromLogdir(flags.logdir, flags.enable_first_N_runs)
     gr_multiplexer = plugin_event_multiplexer.EventMultiplexer(run_path_map=run_path_map,
                                                                 size_guidance=size_guidance,
                                                                 tensor_size_guidance=None,
@@ -33,6 +33,10 @@ def gr_tensorboard_wsgi(flags, plugin_loaders, assets_zip_provider):
     return application.TensorBoardWSGI(plugins, flags.path_prefix)
 
 def _getRunPathMapFromLogdir(logdir, most_recent_num):
+    if most_recent_num >= 0:
         dir_list = sorted(list(io_wrapper.GetLogdirSubdirectories(logdir)), key=os.path.getmtime)
         num_files = min(most_recent_num, len(dir_list))
         return {os.path.relpath(path, logdir): path for path in dir_list[-num_files:]}
+    else:
+        dir_list = list(io_wrapper.GetLogdirSubdirectories(logdir))
+        return {os.path.relpath(path, logdir): path for path in dir_list}
